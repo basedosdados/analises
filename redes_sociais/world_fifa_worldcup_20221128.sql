dadbb4fc191903b3b5c25984be87f991b8ce9ff5
Gabrielle Carvalho <77730866+gabrielle-carv@users.noreply.github.com>
2022-12-01 16:55:58 -0300

    Update world_fifa_worldcup_20221128.sql

--- redes_sociais/world_fifa_worldcup_20221128.sql
+++ redes_sociais/world_fifa_worldcup_20221128.sql
@@ -1,23 +1,23 @@
-#Scatter plot taxa de distorção idade-série e taxa de abandono (ensino médio)
-WITH capitais AS (
-SELECT municipio,id_municipio,regiao FROM `basedosdados.br_bd_diretorios_brasil.municipio`
-WHERE capital_uf = 1)
-SELECT t1.municipio,t1.regiao,t2.id_escola,t2.tdi_ensino_medio,t2.taxa_aband_ensino_medio
-FROM capitais t1
-JOIN `basedosdados.br_inep_indicadores_educacionais.escola` t2
-ON t1.id_municipio = t2.id_municipio
-WHERE ano = 2019
-AND localizacao = 'urbana'
-AND rede = 'estadual'
-AND taxa_aband_ensino_medio IS NOT NULL
-# A partir da base gerada foi feito um gráfico no flourish:https://public.flourish.studio/visualisation/5684964/
-
-
-#Estado - ranking média de hora-aula diária em creches públicas
-SELECT sigla_uf,had_educacao_infantil_creche,localizacao,rede
-FROM `basedosdados.br_inep_indicadores_educacionais.uf`
-WHERE ano = 2020
-AND localizacao = 'urbana'
-AND rede = 'publica'
-ORDER BY had_educacao_infantil_creche DESC
-#A partir da base gerada foi feito um gráfico no flourish: https://public.flourish.studio/visualisation/5708673/
+WITH tabela_gol AS (SELECT 
+  year,
+  team_a_name,
+  team_a_score,
+  team_b_score,  
+  team_b_name, 
+  CASE
+      WHEN
+         team_a_name = 'Brazil' THEN team_a_score
+      WHEN 
+         team_b_name = 'Brazil' THEN team_b_score
+  END AS gol_brazil,
+  
+  FROM `basedosdados.world_fifa_worldcup.matches` 
+  WHERE stage_name = 'group stage' AND (team_a_name = 'Brazil'OR team_b_name = 'Brazil'))
+  SELECT 
+      year,
+      sum(gol_brazil) AS total_gol,
+      avg(gol_brazil) AS media_gol,
+      max(gol_brazil) AS max_gol,
+      stddev(gol_brazil) AS dp_gol
+   FROM tabela_gol
+   GROUP BY 1
