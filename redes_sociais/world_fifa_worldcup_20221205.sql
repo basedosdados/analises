ca055ab3aa5cdb6d3be6912fd4dae34f5d6b2f37
Gabrielle Carvalho <77730866+gabrielle-carv@users.noreply.github.com>
2022-12-05 14:44:49 -0300

    Update world_fifa_worldcup_20221205.sql

--- redes_sociais/world_fifa_worldcup_20221205.sql
+++ redes_sociais/world_fifa_worldcup_20221205.sql
@@ -8,3 +8,4 @@ SELECT
   score_penalties 
 FROM `basedosdados.world_fifa_worldcup.matches` 
 WHERE (team_a_name = 'Brazil' OR team_b_name = 'Brazil') AND stage_name = 'round of 16'
+ORDER BY year DESC
