0afb320e64b54fa2db7b98e0f38f9ba7f6471e80
gustavoalcantara <gustavo.ca.geografia@gmail.com>
2023-04-27 12:05:29 -0300

    update br_diretorios_escola_nome_20232704
    adiciona duas novas colunas

--- redes_sociais/br_diretorios_escola_nome_20232704.sql
+++ redes_sociais/br_diretorios_escola_nome_20232704.sql
@@ -1,5 +1,6 @@
-SELECT a.nome, id_escola, endereco, telefone, b.nome as nome_municipio,
+SELECT a.nome, id_escola, endereco, telefone, porte, etapas_modalidades_oferecidas,
+b.nome as nome_municipio
 FROM `basedosdados.br_bd_diretorios_brasil.escola` a
 INNER JOIN `basedosdados.br_bd_diretorios_brasil.municipio` b
 ON a.id_municipio = b.id_municipio
-WHERE b.nome = "Seu municipio" 
+WHERE b.nome = "Campinas"
