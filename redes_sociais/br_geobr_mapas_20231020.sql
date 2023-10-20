c15d49dbe5783a4426e134f0a11379e5c972ed2f
Gustavo Alcantara <gustavo.ca.geografia@gmail.com>
2023-10-20 16:34:25 -0300

    Update br_geobr_mapas_20231020.sql

--- redes_sociais/br_geobr_mapas_20231020.sql
+++ redes_sociais/br_geobr_mapas_20231020.sql
@@ -1,10 +1,10 @@
 --- Polígonos dos territórios indígenas do Brasil
 SELECT * FROM `basedosdados.br_geobr_mapas.terra_indigena` 
 
---- Latitude e longitude de todas as escolas brasileiras até 2020
+--- Latitude e longitude de todas as escolas brasileiras de 2020
 SELECT * FROM `basedosdados.br_geobr_mapas.escola`
 WHERE sigla_uf = 'AC' 
 
---- Latitude e Longitude dos estabelecimentos de Saúde
+--- Latitude e Longitude dos estabelecimentos de Saúde de 2015
 SELECT * FROM `basedosdados.br_geobr_mapas.estabelecimentos_saude` 
 WHERE sigla_uf = 'AC'
